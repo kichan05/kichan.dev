15d35637a49f3c80bcd50230a0d0dc3d3e8f40fe
바키찬 <ckstmznf@naver.com>
2023-12-04 00:34:24 +0900

    fix:detail

--- src/component/BusinessCardSection.js
+++ src/component/BusinessCardSection.js
@@ -1,6 +1,6 @@
 import style from "./businessCardSection.css"
 
-import { useState } from "react";
+import {useState} from "react";
 import edcanLogo from "../assets/edcan.svg";
 import edcanTypo from "../assets/edcan_typo.svg";
 
@@ -9,36 +9,37 @@ export function BusinessCardSection() {
   const [businessCardXDeg, setBusinessCardXDeg] = useState(0.0)
   const [businessCardYDeg, setBusinessCardYDeg] = useState(0.0)
 
+  function cardMouseMove(event) {
+    const {left, top, width, height} = event.currentTarget.getBoundingClientRect()
+    const {clientX, clientY} = event
+    const x = clientX - left
+    const y = clientY - top
+
+    const halfWidth = width / 2
+    const halfHeight = height / 2
+
+    const xHalfDistance = Math.abs(halfWidth - x)
+    const yHalfDistance = Math.abs(halfHeight - y)
+
+    let xDegree = 30 * xHalfDistance / halfWidth * (y < halfHeight ? 1 : -1)
+    let yDegree = 30 * yHalfDistance / halfHeight * (x < halfWidth ? 1 : -1)
+
+    setBusinessCardXDeg(xDegree)
+    setBusinessCardYDeg(yDegree)
+  }
+
   return <section style={style} className="business-card-section full-section">
     <div className={`business-card-wrap ${isReverse ? "reverse" : ""}`}
-         style={{transform : `rotateX(${businessCardXDeg}deg) rotateY(${businessCardYDeg}deg)`}}
-         onMouseMove={(event) => {
-           const {left, top, width, height} = event.currentTarget.getBoundingClientRect()
-           const {clientX, clientY} = event
-           const x = clientX - left
-           const y = clientY - top
-
-           const halfWidth = width / 2
-           const halfHeight = height / 2
-
-           const xHalfDistance = Math.abs(halfWidth - x)
-           const yHalfDistance = Math.abs(halfHeight - y)
-
-           let xDegree = 30 * xHalfDistance / halfWidth * (y < halfHeight ? 1 : -1)
-           let yDegree = 30 * yHalfDistance / halfHeight * (x < halfWidth ? 1 : -1)
-
-           setBusinessCardXDeg(xDegree)
-           setBusinessCardYDeg(yDegree)
-         }}
+         style={{transform: `rotateX(${businessCardXDeg}deg) rotateY(${businessCardYDeg}deg)`}}
+         onMouseMove={cardMouseMove}
          onMouseOut={() => {
            setBusinessCardXDeg(0)
            setBusinessCardYDeg(0)
          }}
-      >
+    >
       <div className="front">
         <div className="edcan-logo-wrap">
-          {isReverse}
-          <img src={edcanLogo}/>
+          <img src={edcanLogo} alt="EDCAN 로고"/>
         </div>
         <div className="name-wrap">
           <h2>박희찬</h2>
@@ -46,16 +47,25 @@ export function BusinessCardSection() {
         </div>
         <div className="content-wrap">
           <div className="tel-wrap">
-            <div className="tel-title title">TEL</div>
-            <div className="tel-content content">010-0000-0000</div>
+            <div className="tel-title title">학력</div>
+            <div className="tel-content content">선린인터넷고등학교</div>
+            <div className="tel-content content">소프트웨어과 재학 (2021~)</div>
           </div>
           <div className="mail-wrap">
-            <div className="mail-title title">MAIL</div>
-            <div className="mail-content content">ckstmznf@naver.com</div>
+            <div className="mail-title title">
+              MAIL
+            </div>
+            <div className="mail-content content">
+              <span className="pointer" onClick={() => {
+                navigator.clipboard.writeText("ckstmznf@naver.com")
+              }}>ckstmznf@naver.com</span>
+            </div>
           </div>
           <div className="link-wrap">
             <div className="link-title title">GITHUB</div>
-            <div className="link-content content">github.com/kichan05</div>
+            <div className="link-content content">
+              <a href="https://www.github.com/kichan05" target="_blank">github.com/kichan05</a>
+            </div>
           </div>
         </div>
         <div className="dots">
@@ -72,13 +82,15 @@ export function BusinessCardSection() {
         <div className="circle"></div>
       </div>
       <div className="back">
-        <img src={edcanTypo}/>
+        <img src={edcanTypo} alt="EDCAN 로고"/>
       </div>
     </div>
 
-
     <div className="btn revers-card"
-      onClick={() => {setIsReverse(!isReverse)}}
-    >명함 뒤집기</div>
+         onClick={() => {
+           setIsReverse(!isReverse)
+         }}
+    >명함 뒤집기
+    </div>
   </section>;
 }
