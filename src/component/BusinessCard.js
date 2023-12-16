ed61e3527b0cc5e60bbda7224ef23bd686530814
바키찬 <ckstmznf@naver.com>
2023-12-16 20:29:12 +0900

    fix:명함 로직 수정

---
+++ src/component/BusinessCard.js
@@ -0,0 +1,60 @@
+import classNames from "classnames";
+import edcanLogo from "../assets/edcan.svg";
+import edcanTypo from "../assets/edcan_typo.svg";
+
+export const BusinessCard = ({cardState, onMouseMove, onMouseOut, onEmailCopy}) => {
+  return (
+    <div
+      className={classNames("business-card-wrap", {reverse: cardState.isReverse})}
+      style={{
+        transform: `perspective(2000px) rotateX(${cardState.xDeg}deg) rotateY(${cardState.yDeg}deg)`
+      }}
+      onMouseMove={onMouseMove}
+      onMouseOut={onMouseOut}>
+      <div className="front">
+        <div className="edcan-logo-wrap">
+          <img src={edcanLogo} alt="EDCAN 로고"/>
+        </div>
+        <div className="name-wrap">
+          <h2>박희찬</h2>
+          <h3>Developer</h3>
+        </div>
+        <div className="content-wrap">
+          <div className="tel-wrap">
+            <div className="tel-title title">학력</div>
+            <div className="tel-content content">선린인터넷고등학교</div>
+            <div className="tel-content content">소프트웨어과 재학 (2021~)</div>
+          </div>
+          <div className="mail-wrap">
+            <div className="mail-title title">
+              MAIL
+            </div>
+            <div className="mail-content content">
+              <span className="pointer" onClick={onEmailCopy}>ckstmznf@naver.com</span>
+            </div>
+          </div>
+          <div className="link-wrap">
+            <div className="link-title title">GITHUB</div>
+            <div className="link-content content">
+              <a href="https://www.github.com/kichan05" target="_blank">github.com/kichan05</a>
+            </div>
+          </div>
+        </div>
+        <div className="dots">
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
+        </div>
+        <div className="bottom">
+          <img src={edcanTypo} alt=""/>
+        </div>
+        <div className="circle"></div>
+      </div>
+      <div className="back">
+        <img src={edcanTypo} alt="EDCAN 로고"/>
+      </div>
+    </div>
+  )
+}
--- src/section/BusinessCardSection.js
+++ src/section/BusinessCardSection.js
@@ -1,15 +1,14 @@
 import style from "./businessCardSection.css"
 
 import {useContext, useState} from "react";
-import edcanLogo from "../assets/edcan.svg";
-import edcanTypo from "../assets/edcan_typo.svg";
 
 import {UiDispatch} from "../App";
+import {BusinessCard} from "../component/BusinessCard";
 
 export function BusinessCardSection() {
   const uiDispatch = useContext(UiDispatch)
 
-  const [businessCardState, setBusinessCardState] = useState({
+  const [cardState, setCardState] = useState({
     isReverse: false, xDeg: 0.0, yDeg: 0.0
   })
 
@@ -20,90 +19,42 @@ export function BusinessCardSection() {
     const x = clientX - left
     const y = clientY - top
 
-    const xDeg = Math.round(-25 + 50 / height * y)
-    const yDeg = Math.round(25 - 50 / width * x)
+    const xDeg = Math.round(-20 + 40 / height * y)
+    const yDeg = Math.round(20 - 40 / width * x)
 
+    setCardState({...cardState, xDeg, yDeg})
+  }
 
-    setBusinessCardState(prev => {
-      return {...prev, xDeg, yDeg}
+  const emailCopy = () => {
+    uiDispatch({
+      type: "TOAST_APPEND",
+      toast: {
+        title: "이메일 복사 완료",
+        message: "이메일이 복사 되었습니다.",
+      },
     })
+    navigator.clipboard.writeText("ckstmznf@naver.com")
   }
 
   return (
-    <section style={style} className="business-card-section full-section">
-      <div
-        className={`business-card-wrap ${businessCardState.isReverse ? "reverse" : ""}`}
-        style={{transform: `perspective(2000px) rotateX(${businessCardState.xDeg}deg) rotateY(${businessCardState.yDeg}deg)`}}
+    <section
+      style={style}
+      className="business-card-section full-section">
+
+      <BusinessCard
+        cardState={cardState}
         onMouseMove={cardMouseMove}
         onMouseOut={() => {
-          setBusinessCardState(prev => (
-            {...prev, xDeg: 0, yDeg: 0}
-          ))
-        }}>
-        <div className="front">
-          <div className="edcan-logo-wrap">
-            <img src={edcanLogo} alt="EDCAN 로고"/>
-          </div>
-          <div className="name-wrap">
-            <h2>박희찬</h2>
-            <h3>Developer</h3>
-          </div>
-          <div className="content-wrap">
-            <div className="tel-wrap">
-              <div className="tel-title title">학력</div>
-              <div className="tel-content content">선린인터넷고등학교</div>
-              <div className="tel-content content">소프트웨어과 재학 (2021~)</div>
-            </div>
-            <div className="mail-wrap">
-              <div className="mail-title title">
-                MAIL
-              </div>
-              <div className="mail-content content">
-              <span className="pointer" onClick={() => {
-                uiDispatch({
-                  type: "TOAST_APPEND",
-                  toast: {
-                    title: "이메일 복사 완료",
-                    message: "이메일 복사됨",
-                  },
-                })
-                navigator.clipboard.writeText("ckstmznf@naver.com")
-              }}>ckstmznf@naver.com</span>
-              </div>
-            </div>
-            <div className="link-wrap">
-              <div className="link-title title">GITHUB</div>
-              <div className="link-content content">
-                <a href="https://www.github.com/kichan05" target="_blank">github.com/kichan05</a>
-              </div>
-            </div>
-          </div>
-          <div className="dots">
-            <div className="dot"></div>
-            <div className="dot"></div>
-            <div className="dot"></div>
-            <div className="dot"></div>
-            <div className="dot"></div>
-          </div>
-          <div className="bottom">
-            <img src={edcanTypo} alt=""/>
-          </div>
+          setCardState({...cardState, xDeg: 0, yDeg: 0})
+        }}
+        onEmailCopy={emailCopy}
+      />
 
-          <div className="circle"></div>
-        </div>
-        <div className="back">
-          <img src={edcanTypo} alt="EDCAN 로고"/>
-        </div>
-      </div>
-
-      <div className="btn revers-card"
-           onClick={() => {
-             setBusinessCardState(prev => {
-               return {...prev, isReverse: !prev.isReverse}
-             })
-           }}
-      >명함 뒤집기
-      </div>
+      <div
+        className="btn revers-card"
+        onClick={() => {
+          setCardState({...cardState, isReverse: !cardState.isReverse})
+        }}>명함 뒤집기</div>
     </section>
   )
 }
