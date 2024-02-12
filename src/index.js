81aac669272fdbd70a18ea2d5254c6125c87591d
바키찬 <ckstmznf@naver.com>
2024-02-13 00:49:20 +0900

    Add vercel analytics

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { inject } from "@vercel/analytics"
+
+inject()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
