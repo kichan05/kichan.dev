d9669bdea68ead5d032135d32d62cb5bf640634a
바키찬 <ckstmznf@naver.com>
2024-02-14 22:48:20 +0900

    Portfolio 내용

--- src/section/ProfileSection.js
+++ src/section/ProfileSection.js
@@ -12,6 +12,8 @@ const ProfileSectionStyle = styled.section`
   }
 
   ul {
+    font-size: 14px;
+    
     padding: 0 8px;
     margin-top: 24px;
 
@@ -51,14 +53,14 @@ const ProfileList = ({title, date, children}) => {
       padding-left: 30px;
 
       margin-top: 5px;
-      font-size: 1.05em;
+      font-size: 1.1em;
 
       line-height: 19px;
     }
 
     a {
       color: ${p => p.theme.color.HeechanBlue};
-      font-weight: 600;
+      font-weight: 500;
       transition: 100ms;
     }
 
@@ -87,6 +89,40 @@ export const ProfileSection = () => {
       title: "선린 116기 소프트웨어과 졸업",
       date: "2021.03 ~ 2024.02",
     },
+    {
+      title: "EDCAN 8기 부장",
+      date: "2021.11 ~ 2022.11",
+      description: <>
+        모바일콘텐츠 개발반 <a href={"https://edcan.kr"} target={"_blank"}>EDCAN</a>의 8기 부부장으로 활동했습니다.
+        <br/>
+        동아리를 총괄하고 수업 준비, 행사 준비를 하면서 1년동안 동아리를 이끌어 갔습니다.
+        <br/><br/>
+        <a href="https://velog.io/@ckstmznf/지난-1년간-전공동아리를-이끌어보았다" target={"_blank"}>동아리 부장 회고록</a>을 참고해주세요
+      </>
+    },
+    {
+      title: "2022 소프트웨어 나눔 축제 캠프 총괄",
+      date: "2022.04 ~ 2022.08",
+      description: <>
+        선린인터넷고등학교가 주관, 주최하는 2022 소프트웨어 나눔축제의 캠프 총괄로 활동했습니다.
+        <br/>
+        EDCAN 캠프(AndroidStudio로 나만의 일기장 앱 만들기)를 운영했습니다.
+        <br/><br/>
+        <a href="https://velog.io/@ckstmznf/SSF2022" target={"_blank"}>캠프 총괄 회고록</a>을 참고해주세요
+      </>
+    },
+    {
+      title: "제 8회 선린 해커톤 동상 수상",
+      data: "2022.07",
+    },
+    {
+      title: "2022 선린 디지털 콘텐츠 경진대회 동상 수상",
+      data: "2022.12",
+    },
+    {
+      title: "2023 선린 천하제일코딩대회 디자인 총괄",
+      data: "2023.04 ~ 2023.07",
+    }
   ]
 
   return (
@@ -98,16 +134,6 @@ export const ProfileSection = () => {
             title={p.title}
             date={p.date}>{p.description}</ProfileList>
         ))}
-
-        {/*<ProfileList*/}
-        {/*  title={"EDCAN 8기 부장"}*/}
-        {/*  date={"2021.11 ~ 2022.11"}>*/}
-        {/*  모바일콘텐츠 개발반 EDCAN의 8기 부부장으로 활동했습니다.*/}
-        {/*  <br/>*/}
-        {/*  동아리를 총괄하고 수업 준비, 행사 준비를 하면서 1년동안 동아리를 이끌어 갔습니다.*/}
-        {/*  <br/><br/>*/}
-        {/*  <a href="https://velog.io/@ckstmznf/지난-1년간-전공동아리를-이끌어보았다" target={"_blank"}>동아리 부장 회고록</a>을 참고해주세요*/}
-        {/*</ProfileList>*/}
       </ul>
     </ProfileSectionStyle>
   )
