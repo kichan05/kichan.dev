71c3242b07070fbde3668ed70c2e2a9e1d87b063
바키찬 <ckstmznf@naver.com>
2024-03-11 22:01:06 +0900

    Project Item

--- src/section/ProjectSection.js
+++ src/section/ProjectSection.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 import Membeder from "./../assets/Membeder 목업.png"
+import {CgChevronRight} from "react-icons/cg";
 
 const ProjectSectionStyle = styled.section`
   padding: 40px 24px;
@@ -15,41 +16,60 @@ const ProjectSectionStyle = styled.section`
     color: ${p => p.theme.color.Gray9};
     font-size: 2em;
   }
-  
+
   ul {
+    margin-top: 24px;
+
     display: grid;
     grid-template-columns: repeat(2, 1fr);
     gap: 8px;
   }
-  
+
   .project-item {
     aspect-ratio: 4 / 3;
+    border: 1px solid ${p => p.theme.color.Gray2};
     border-radius: 24px;
     overflow: hidden;
-    
+
     display: flex;
     flex-direction: column;
-    
+
+    cursor: pointer;
+
     .thumbnail {
       flex: 1;
-      
+
       overflow: hidden;
-      
+
       img {
-        width: 100%;
+        height: 100%;
         object-fit: cover;
-        
-        transition: 100ms;
+
+        transition: 200ms;
       }
     }
-    
+
     .description {
-      background-color: ${p => p.theme.color.Gray4};
+      color: ${p => p.theme.color.Gray8};
+      background-color: ${p => p.theme.color.Gray3};
       padding: 16px 12px;
+      
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      
+      p {
+        margin-top: 4px;
+      }
+
+      .goto-project {
+        color: ${p => p.theme.color.Gray7};
+        font-size: 2em;
+      }
     }
-    
+
     &:hover img {
-      transform: scale(1.2);
+      transform: scale(1.1);
     }
   }
 `
@@ -61,15 +81,20 @@ export const ProjectSection = () => {
         <h2>제가 만든 프로젝트들이에요</h2>
 
         <ul>
-          <li className={"project-item"}>
-            <div className="thumbnail">
-              <img src={Membeder} alt=""/>
-            </div>
-            <div className={"description"}>
-              <h4>Membeder</h4>
-              <p>IT 직군 사람들을 위한 팀빌딩 어플리케이션<br/>프로젝트 팀장과 Android개발을 담당했습니다.</p>
-            </div>
-          </li>
+          {[1, 2, 3].map((i) => (
+            <li className={"project-item"} key={i}>
+              <div className="thumbnail">
+                <img src={Membeder} alt=""/>
+              </div>
+              <div className={"description"}>
+                <div>
+                  <h4>Membeder</h4>
+                  <p>IT 직군 사람들을 위한 팀빌딩 어플리케이션<br/>프로젝트 팀장과 Android개발을 담당했습니다.</p>
+                </div>
+                <CgChevronRight className={"goto-project"}/>
+              </div>
+            </li>
+          ))}
         </ul>
 
       </div>
